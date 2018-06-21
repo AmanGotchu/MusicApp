7b57affc37818d5dfce55d87585a574dd972af55
Andy Prevalsky <andyprevalsky@yahoo.com>
2018-06-21 14:11:54 -0500

    spotify authentication complete, (caution: unsercure)

--- MusicApp/src/components/SpotifyLogin.js
+++ MusicApp/src/components/SpotifyLogin.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, Linking } from 'react-native';
+import { Text, Linking, View } from 'react-native';
 import querystring from 'querystring';
 
 const S_CLIENT_ID = '5a7e235500fe40509dee5c659b63f316';
@@ -12,27 +12,59 @@ const extension = querystring.stringify({
   scope: 'user-read-private user-read-email'
 });
 const url = `https://accounts.spotify.com/authorize/?${extension}`;
-let code;
+const Buffer = require('buffer/').Buffer;
 
 class SpotifyLogin extends Component {
+  state = { refreshToken: '', accessToken: '' }
   componentWillMount() {
     Linking.openURL(url).catch(err => console.error('an error as occured', err));
   }
 
   componentDidMount() {
-    Linking.addEventListener('url', this.handleOpenURL);
+    Linking.addEventListener('url', this.handleOpenURL.bind(this));
   }
   componentWillUnmount() {
     Linking.removeEventListener('url', this.handleOpenURL);
   }
 
+  setTokens(JsonData) {
+    this.setState({ accessToken: JsonData.access_token, refreshToken: JsonData.refresh_token });
+  }
+
   handleOpenURL(event) {
-    code = event.url.split('=').pop();
-    console.log(code);
+    if (event.url.includes('error')) {
+      console.error('Denied Spotify Authentication');
+      Linking.openURL(url).catch(err => console.error('an error as occured', err));
+    } else {
+      const code = event.url.split('=').pop();
+      fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: 'Basic ' + (new Buffer(S_CLIENT_ID + ':' + S_CLIENT_SECRET).toString('base64'))
+        },
+        body: querystring.stringify({
+          code,
+          redirect_uri: REDIRECT_URI,
+          grant_type: 'authorization_code'
+        }),
+        json: true
+      }).then((response) => response.json())
+        .then((Jresponse) => this.setTokens(Jresponse));
+    }
   }
 
   render() {
-    return (<Text> Verified Page </Text>);
+    return (
+      <View style={{ flex: 1, alignContent: 'center' }}>
+        <Text> 
+          Your Access Token is: {this.state.accessToken} 
+        </Text >
+        <Text> 
+          Your Refresh Token is: {this.state.refreshToken} 
+        </Text>  
+      </View>
+    );
   }
 }
 
