8ad29772934142c4bfe59763cc4795dc2fb109fd
Andy Prevalsky <andyprevalsky@yahoo.com>
2018-07-12 21:22:53 -0500

    show next and previous songs

--- MusicApp/src/components/ManageHub.js
+++ MusicApp/src/components/ManageHub.js
@@ -33,8 +33,42 @@ class ManageHub extends Component {
                 })
             }).then(response => response.json().then(
                 songInfo => this.props.setCurrentSong(songInfo)))
-            .catch((err) => console.log(err));
+            .catch(err => console.log(err));
         });
+    }
+
+    getNextSong() {
+        const userId = firebase.auth().currentUser.uid;
+        fetch('http://127.0.0.1:5000/hubs/getNextSong', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: querystring.stringify({
+                userId,
+                hubId: this.props.hubId
+            })
+        }).then(response => response.json().then(
+            songInfo => this.props.setCurrentSong(songInfo)
+        ))
+        .catch(err => console.log(err));
+    }
+
+    getPreviousSong() {
+        const userId = firebase.auth().currentUser.uid;
+        fetch('http://127.0.0.1:5000/hubs/getPreviousSong', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: querystring.stringify({
+                userId,
+                hubId: this.props.hubId
+            })
+        }).then(response => response.json().then(
+            songInfo => this.props.setCurrentSong(songInfo)
+        ))
+        .catch(err => console.log(err));
     }
 
     renderBack() {
@@ -50,13 +84,12 @@ class ManageHub extends Component {
         );
     }
 
-    renderCenterComponent() {
+    renderAlbumCover() {
         if (this.props.currSongInfo) {
-            console.log(this.props.currSongInfo.album);
             return (
                 <Image
                     source={{ url: this.props.currSongInfo.album.images[0].url }}
-                    style={{ height: 300, width: 300 }}
+                    style={{ height: 200, width: 200, alignSelf: 'center' }}
                 />
             );
         }
@@ -66,8 +99,38 @@ class ManageHub extends Component {
         );
     }
 
+    renderArtistName() {
+        if (this.props.currSongInfo) {
+            console.log(this.props.currSongInfo.artists[0].name);
+            return (
+                <Text
+                    style={{ alignSelf: 'center', color: 'white' }}
+                > 
+                    {this.props.currSongInfo.artists[0].name} 
+                </Text>
+            );
+        }
+        return (
+            <Text> No Name Available </Text>
+        );
+    }
+
+    renderSongName() {
+        if (this.props.currSongInfo) {
+            console.log(this.props.currSongInfo.name);
+            return (
+                <Text
+                    style={{ alignSelf: 'center', color: 'white' }}
+                > 
+                    {this.props.currSongInfo.name} 
+                </Text>
+            );
+        }
+        return (
+            <Text> No Name Available </Text>
+        );
+    }
     render() {
-        console.log(this.renderCenterComponent(), 'render');
         return (
             <View style={{ flex: 1, backgroundColor: 'grey' }}>
                 <Header
@@ -75,8 +138,26 @@ class ManageHub extends Component {
                     backgroundColor='blue'
                 />
                 <Text> Hello </Text>
-                <View style={{ backgroundColor: 'black', flex: 1 }} >
-                    {this.renderCenterComponent()}
+                <View style={{ backgroundColor: 'black', flex: 1, justifyContent: 'center' }} >
+                    <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+                        <Icon
+                            name='controller-jump-to-start'
+                            onPress={() => this.getPreviousSong()}
+                            type='entypo'
+                            color='white'
+                            underlayColor='rgba(0, 0, 0, .0)'
+                        />
+                        {this.renderAlbumCover()}
+                        <Icon
+                            name='controller-next'
+                            onPress={() => this.getNextSong()}
+                            type='entypo'
+                            color='white'
+                            underlayColor='rgba(0, 0, 0, .0)'
+                        />
+                    </View>
+                    {this.renderArtistName()}
+                    {this.renderSongName()}
                 </View>
             </View>
         );
